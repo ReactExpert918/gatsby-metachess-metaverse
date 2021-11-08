e8883a9d5e1aabdb3d70b2bc69b6412856a54528
Manish Runwal <manishrunwal66@gmail.com>
2021-11-08 22:28:54 +0530

    Chess game choose page with header.
    Signed-off-by: Manish Runwal <manishrunwal66@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,35 @@
-import React, { useState, useContext, useEffect } from "react";
-import ChoseModeSection from "../components/ChoseModeSection";
-import PlayWithAISection from "../components/PlayWithAISection";
-import { MODES, AI_PLAY_MODE } from "../constants/playModes";
-import { connect } from "react-redux";
-import { Actions as GameplayActions } from "../store/gameplay/gameplay.action";
-import { Actions as UserActions } from "../store/user/user.action";
 import { navigate } from "gatsby";
-import PlayWithHumanSection from "../components/PlayWithHumanSection";
-import { IAppState } from "../store/reducers";
-import { IServerStatus } from "../store/user/user.interfaces";
-import { isSSR } from "../lib/utils";
+import React, { useState } from "react";
+import BorderedGameButton from "../components/BorderedGameButton";
+import Chessboard2D from "../lib/svgIcons/Chessboard2D";
+import Chessboard3D from "../lib/svgIcons/Chessboard3D";
+import Chessboard3DWarF from "../lib/svgIcons/Chessboard3DWarF";
 
-interface IActionProps {
-  setPlayMode: typeof GameplayActions.setPlayMode;
-  setPlayerColor: typeof GameplayActions.setPlayerColor;
-  setChoseMode: typeof UserActions.setChoseMode;
-  setGameStartDate: typeof GameplayActions.setGameStartDate;
-}
-
-interface ISelectChooseModeProps {
-  serverStatus: IServerStatus;
-  choseMode: MODES;
-}
-
-const ChoseMode = (props: ISelectChooseModeProps & IActionProps) => {
-  const setAIPlayMode = (playMode: AI_PLAY_MODE) => {
-    props.setPlayMode({
-      isAI: true,
-      isHumanVsHuman: false,
-      aiMode: playMode,
-    });
-    props.setGameStartDate(new Date().getTime());
-    props.setPlayerColor(Math.random() >= 0.5 ? "b" : "w");
-    navigate("/game");
-  };
-
-  const onJoinRoom = (roomName: string) => {
-    props.setPlayMode({
-      isHumanVsHuman: true,
-      roomName,
-      isCreate: false,
-    });
-    navigate("/game");
-  };
-  const onGoBack = () => {
-    props.setChoseMode(MODES.CHOSE_MODE);
-  };
-
-  const { choseMode } = props;
-
+const ChooseGame = () => {
   return (
-    <div
-      className="choseModeContainer m-0-auto"
-    >
-      {choseMode === MODES.CHOSE_MODE && <ChoseModeSection setMode={props.setChoseMode} />}
-      {choseMode === MODES.PLAY_AI && (
-        <PlayWithAISection setPlayMode={setAIPlayMode} goBack={onGoBack} />
-      )}
-      {choseMode === MODES.PLAY_WITH_HUMAN && (
-        <PlayWithHumanSection goBack={onGoBack} onJoinRoom={onJoinRoom} />
-      )}
+    <div className={"choseModeSectionContainer"}>
+      <div className={"headerWrapper"}>
+        <p className="header-heading">GAME</p>
+      </div>
+      <div className="borderedButtonWrapper">
+        <BorderedGameButton
+          GameIcon={<Chessboard2D className="game-2d-icon" />}
+          title="2D"
+          available
+          onPlayClick={()=>navigate('/choose-mode')}
+        />
+        <BorderedGameButton
+          GameIcon={<Chessboard3D className="game-3d-icon" />}
+          title="3D"
+        />
+        <BorderedGameButton
+          GameIcon={<Chessboard3DWarF className="game-3df-icon" />}
+          title="3D"
+          subtitle="War-front"
+        />
+      </div>
     </div>
   );
 };
 
-const mapStateToProps = (state: IAppState) => ({
-  serverStatus: state.user.serverStatus,
-  choseMode: state.user.choseMode,
-});
-
-const connected = connect<ISelectChooseModeProps, IActionProps>(
-  mapStateToProps as any,
-  {
-    setPlayMode: GameplayActions.setPlayMode,
-    setPlayerColor: GameplayActions.setPlayerColor,
-    setChoseMode: UserActions.setChoseMode,
-    setGameStartDate: GameplayActions.setGameStartDate,
-  }
-)(ChoseMode);
-
-export default connected;
+export default ChooseGame;
