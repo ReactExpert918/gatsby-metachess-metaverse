10e37a77cf8a4ad4603d9742c9db72d531e1ff22
ReactExpert918 <potterharrry0918@gmail.com>
2021-11-15 17:25:31 -0500

    socket.io integration for the friend-status

--- src/components/FriendsSidebar/FriendList.tsx
+++ src/components/FriendsSidebar/FriendList.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import SocketService from "../../services/socket.service";
 import { chatActions } from "../../store/chat/chat.actions";
 import { IFriend } from "../../store/chat/chat.interfaces";
 import { IAppState } from "../../store/reducers";
 import FriendListItem from "./FriendListItem";
 import NoRecord from "./NoRecord";
+
+export interface ISocketFriend {
+  Id: string;
+  Online?: boolean;
+}
 
 const FriendList = (): JSX.Element => {
   const { friendsList } = useSelector((state: IAppState) => state.chat);
@@ -12,9 +18,29 @@ const FriendList = (): JSX.Element => {
   const showAddFriend = () => {
     dispatch(chatActions.toggleAddFriendSearch(true));
   };
+
+  const updateFriendStatus = (friend: ISocketFriend) => {
+    const friendIndex = friendsList.findIndex((f) => f.Id === friend.Id);
+    friendsList[friendIndex].Account.IsOnline = friend.Online;
+    dispatch(chatActions.setFriendsList(friendsList));
+  };
+
   useEffect(() => {
     dispatch(chatActions.fetchFriendsList());
   }, []);
+
+  useEffect(() => {
+    if (friendsList.length === 0) {
+      return;
+    }
+
+    SocketService.subscribeTo({
+      eventName: "friend-status",
+      callback: (friend: ISocketFriend) => {
+        updateFriendStatus(friend);
+      },
+    });
+  }, [friendsList]);
 
   return (
     <>
