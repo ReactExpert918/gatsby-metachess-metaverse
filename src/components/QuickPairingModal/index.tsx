dafa0cf56d1593a4d4bbdc0ac996619f484b2143
jasonjin220 <jasonjin220@gmail.com>
2021-12-07 10:31:08 -0500

    add code for cursor pointer in quick play boxes

--- src/components/QuickPairingModal/index.tsx
+++ src/components/QuickPairingModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { connect } from "react-redux";
 import Modal from "../Modal";
 import { Actions as GameplayActions } from "../../store/gameplay/gameplay.action";
@@ -24,20 +24,8 @@ interface IActionProps {
 const QuickPairingModal = (props: IProps) => {
   const toastContext = useContext(ToastContext);
 
-  useEffect(() => {
-    SocketService.subscribeTo({
-      eventName: "cancel-quickplay",
-      callback: () => {
-        console.log("cancel call2");
-        toastContext.hideToast();
-        props.clear();
-      },
-    });
-  }, []);
-
   const onCancel = () => {
     SocketService.sendData("cancel-quickplay", null, () => {
-      console.log("cancel ");
       toastContext.hideToast();
       props.clear();
     });
@@ -79,28 +67,28 @@ const QuickPairingModal = (props: IProps) => {
         <div className="row-boxes">
           <div className="row">
             <div
-              className="box"
+              className="box quick-box"
               onClick={() => onQuickPlay(1, 0, GameType.Bullet)}
             >
               <p>1+0</p>
               <p>bullet</p>
             </div>
             <div
-              className="box"
+              className="box quick-box"
               onClick={() => onQuickPlay(1, 3, GameType.Bullet)}
             >
               <p>1+3</p>
               <p>bullet</p>
             </div>
             <div
-              className="box"
+              className="box quick-box"
               onClick={() => onQuickPlay(2, 1, GameType.Bullet)}
             >
               <p>2+1</p>
               <p>bullet</p>
             </div>
             <div
-              className="box"
+              className="box quick-box"
               onClick={() => onQuickPlay(3, 0, GameType.Blitz)}
             >
               <p>3+0</p>
@@ -111,26 +99,26 @@ const QuickPairingModal = (props: IProps) => {
             className="row"
             onClick={() => onQuickPlay(3, 2, GameType.Blitz)}
           >
-            <div className="box">
+            <div className="box quick-box">
               <p>3+2</p>
               <p>blitz</p>
             </div>
             <div
-              className="box"
+              className="box quick-box"
               onClick={() => onQuickPlay(5, 0, GameType.Blitz)}
             >
               <p>5+0</p>
               <p>blitz</p>
             </div>
             <div
-              className="box"
+              className="box quick-box"
               onClick={() => onQuickPlay(5, 2, GameType.Blitz)}
             >
               <p>5+2</p>
               <p>blitz</p>
             </div>
             <div
-              className="box"
+              className="box quick-box"
               onClick={() => onQuickPlay(10, 0, GameType.Rapid)}
             >
               <p>10+0</p>
@@ -141,25 +129,25 @@ const QuickPairingModal = (props: IProps) => {
             className="row"
             onClick={() => onQuickPlay(0, 0, GameType.Bullet)}
           >
-            <div className="box lg">
+            <div className="box lg quick-box">
               <p>ANY BULLET</p>
             </div>
             <div
-              className="box lg"
+              className="box lg quick-box"
               onClick={() => onQuickPlay(0, 0, GameType.Blitz)}
             >
               <p>ANY BLITZ</p>
             </div>
           </div>
           <div
-            className="row"
+            className="row quick-box"
             onClick={() => onQuickPlay(0, 0, GameType.Rapid)}
           >
             <div className="box lg">
               <p>ANY RAPID</p>
             </div>
             <div
-              className="box lg"
+              className="box lg quick-box"
               onClick={() => onQuickPlay(0, 0, GameType.Classical)}
             >
               <p>ANY CLASSICAL</p>
