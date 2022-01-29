c8cdc7f400775e73981a222b11d97b26dc571534
Mitul Pancholi <mitul.pancholi@live.com>
2022-01-29 17:38:11 +0530

    Maintenance Model done

--- src/components/MaintenanceModal/index.tsx
+++ src/components/MaintenanceModal/index.tsx
@@ -5,22 +5,21 @@ import { useSelector } from "react-redux";
 import { IAppState } from "../../store/reducers";
 import { MAINTENANCE_MODE } from "../../store/user/user.interfaces";
 
-
 const MaintenanceModal = () => {
   const { serverStatus } = useSelector((state: IAppState) => state.user);
-  const [userSeenMaintenance, setUserSeenMaintenance] = useState(false);
+  console.log("this is serverStatus =====> ", serverStatus);
+  const [userSeenMaintenance, setUserSeenMaintenance] = useState(true);
   // const newGamesDisabled =
   //   serverStatus.MaintenanceMode === MAINTENANCE_MODE.NEW_GAMES_DISABLED;
-  useEffect(()=>{
-    setUserSeenMaintenance(false);
-  },[serverStatus]);
+  useEffect(() => {
+    if (serverStatus.MaintenanceMode !== 0) {
+      setUserSeenMaintenance(false);
+    }
+  }, [serverStatus]);
   return (
     <>
       {!userSeenMaintenance && (
-        <Modal
-          onClose={() => setUserSeenMaintenance(true)}
-          withBorder
-        >
+        <Modal onClose={() => setUserSeenMaintenance(true)} withBorder>
           <div className={"maintenance-modal"}>
             <MaintenancePage />
             <div className={"btn-container"}></div>
