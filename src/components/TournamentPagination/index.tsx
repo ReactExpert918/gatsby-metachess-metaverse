a579892766e50cc0e0c2d4eb853a71426a61a592
ReactExpert918 <potterharrry0918@gmail.com>
2022-02-23 17:35:14 +0300

    Updated responsive of tournament page

--- src/components/TournamentPagination/index.tsx
+++ src/components/TournamentPagination/index.tsx
@@ -11,12 +11,12 @@ const TournamentPagination = ({ curpage, onTransition }: Props) => {
       <div className="pagination-buttons">
         <button onClick={() => onTransition(curpage - 1)}>
           <svg width="13" height="21" viewBox="0 0 13 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 1L2 10.5531L12 20" stroke="#CCA66A" stroke-width="2" stroke-linecap="round"/>
+            <path d="M12 1L2 10.5531L12 20" stroke="#CCA66A" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         </button>
         <button onClick={() => onTransition(curpage + 1)}>
           <svg width="13" height="21" viewBox="0 0 13 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 1L11 10.5531L1 20" stroke="#CCA66A" stroke-width="2" stroke-linecap="round"/>
+            <path d="M1 1L11 10.5531L1 20" stroke="#CCA66A" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         </button>
       </div>
