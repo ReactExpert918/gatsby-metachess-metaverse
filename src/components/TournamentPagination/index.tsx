aa84801bfd5cf23861c5f0249bf8ec9ea4b0ebb0
ReactExpert918 <potterharrry0918@gmail.com>
2022-03-10 04:39:19 +0900

    update chessUI

--- src/components/TournamentPagination/index.tsx
+++ src/components/TournamentPagination/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import arrow from "../../assets/images/pagination-arrow.png"
+
 interface Props {
   curpage: number;
   onTransition: () => void;
@@ -10,14 +12,16 @@ const TournamentPagination = ({ curpage, onTransition }: Props) => {
     <div className="paginationContainer">
       <div className="pagination-buttons">
         <button onClick={() => onTransition(curpage - 1)}>
-          <svg width="13" height="21" viewBox="0 0 13 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <img src={arrow} className="arrow-left" />
+          {/* <svg width="13" height="21" viewBox="0 0 13 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 1L2 10.5531L12 20" stroke="#CCA66A" strokeWidth="2" strokeLinecap="round"/>
-          </svg>
+          </svg> */}
         </button>
         <button onClick={() => onTransition(curpage + 1)}>
-          <svg width="13" height="21" viewBox="0 0 13 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <img src={arrow} className="arrow-pagination" />
+          {/* <svg width="13" height="21" viewBox="0 0 13 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1 1L11 10.5531L1 20" stroke="#CCA66A" strokeWidth="2" strokeLinecap="round"/>
-          </svg>
+          </svg> */}
         </button>
       </div>
     </div>
